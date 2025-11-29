a1ba2dab71f2ea14779fd1b89c9a04408e39b613
ayyan <sobiawaheed20@googlemail.com>
2025-11-29 17:33:08 +0000

    Add files via upload

--- rocketgamep1/rg1.py
+++ rocketgamep1/rg1.py
@@ -7,35 +7,41 @@ HEIGHT=500
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 screen.fill("grey")
 imi1=pygame.image.load("rocketgamep1/images/1314202.jpg")
+
 imi2=pygame.image.load("rocketgamep1/images/OIP-removebg-preview.png")
 imis2=pygame.transform.scale(imi2,(110,110))
+imi3=pygame.image.load("rocketgamep1/images/R-removebg-preview.png")
 
 x=200
 y=250
 while True:
-    y=y+0.05
+    y=y+0.03
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             pygame.QUIT()
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT and x>-20:
-              x=x-5
+              x=x-15
               pygame.display.update()
-            if event.key==pygame.K_RIGHT and x>510:
-              x=x+5
+            if event.key==pygame.K_RIGHT and x<400:
+              x=x+15
               pygame.display.update()
             if event.key==pygame.K_UP and y>0:
-              y=y-5
+              y=y-15
               pygame.display.update()
-            if event.key==pygame.K_DOWN and y>510:
-              y=y+5
+            if event.key==pygame.K_DOWN and y<510:
+              y=y+15
               pygame.display.update()
 
     
 
     screen.blit(imi1,(0,0))
     screen.blit(imis2,(x,y))
-
+    if y>530:
+      screen.blit(imi3,(-80,0)) 
+      pygame.display.update()
+      pygame.time.wait(4000)
+      break
       
 
 
